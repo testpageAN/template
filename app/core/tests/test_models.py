600f058bc5bc85f6051ba2d17fbf4f8195a0346c
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-06 22:37:07 +0200

    require email 6-1-24

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -36,4 +36,3 @@ class ModelTests(TestCase):
         """Test that creating a user without an email raises a ValueError."""
         with self.assertRaises(ValueError):
             get_user_model().objects.create_user('', 'test123')
-
