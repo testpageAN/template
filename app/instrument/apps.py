a23f2e74fa7e728e2c2d413abdd16db615db761b
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-08 22:29:39 +0200

    create instrument app 8-1-24

--- app/instrument/apps.py
+++ app/instrument/apps.py
@@ -1,6 +1,6 @@
-from django.apps import AppConfig
+from django.apps import AppConfig   # noqa
 
 
-class InstrumentConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'instrument'
+# class InstrumentConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'instrument'
