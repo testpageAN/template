d4584214e8ff11337cdea11335c3e344fefa1ead
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-08 23:03:35 +0200

    create first templates 8-1-24

--- app/instrument/urls.py
+++ app/instrument/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("instruments", views.instruments, name="instruments-page"),
     # path("instrument/<int:id>"),
-    path("instruments/<slug>", views.instrument_detail, name="instrument-detail-page"),
+    path("instruments/<slug>",
+         views.instrument_detail,
+         name="instrument-detail-page"),
 ]
--- app/instrument/views.py
+++ app/instrument/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 # Create your views here.
 def starting_page(request):
     """Docstring"""
-    pass
+    return render(request, 'instrument/index.html')
 
 
 def instruments(request):
