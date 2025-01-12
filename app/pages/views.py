9be55690d93df4ed18a333e791c526e7d4b19dbd
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-12 13:58:09 +0200

    home and about pages 12-01-2024

--- app/pages/views.py
+++ app/pages/views.py
@@ -1,11 +1,29 @@
 from django.shortcuts import render
 
+from listings.models import Listing
+from foremen.models import Foreman
+
 
 def index(request):
     """Docstring"""
-    return render(request, 'pages/index.html')
+    listings = (
+        Listing.objects.order_by('next_check')
+        .filter(is_active=True)[:10]
+    )
+
+    context = {
+        'listings': listings
+    }
+    return render(request, 'pages/index.html', context)
 
 
 def about(request):
     """Docstring"""
-    return render(request, 'pages/about.html')
+    # Get all foremen
+    foremen = Foreman.objects.order_by('-hire_date')
+
+    context = {
+        'foremen': foremen,
+        # 'mvp_foremen': mvp_foremen  # If any
+    }
+    return render(request, 'pages/about.html', context)
