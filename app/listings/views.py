fd63a90535916f6bbc71f257963c4cb29001f2ee
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-12 12:45:57 +0200

    paginator, order, filter 12-01-2024

--- app/listings/views.py
+++ app/listings/views.py
@@ -1,13 +1,20 @@
+from django.core.paginator import Paginator
 from django.shortcuts import render
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator  # noqa
 
 from .models import Listing
 
 
 def index(request):
     """Docstring"""
-    listings = Listing.objects.all()
+    listings = Listing.objects.order_by('next_check').filter(is_active=True)
+
+    paginator = Paginator(listings, 5)
+    page = request.GET.get('page')
+    paged_listings = paginator.get_page(page)
+
     context = {
-        'listings': listings
+        'listings': paged_listings
     }
     return render(request, 'listings/listings.html', context)
 
