0f957b3c74eac3816fda033081b60d81731d24a7
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-12 12:12:57 +0200

    initial listings template actions 12-01-2024

--- app/listings/views.py
+++ app/listings/views.py
@@ -1,14 +1,20 @@
 from django.shortcuts import render
 
+from .models import Listing
+
 
 def index(request):
     """Docstring"""
-    return render(request, 'listings/listings.html')
+    listings = Listing.objects.all()
+    context = {
+        'listings': listings
+    }
+    return render(request, 'listings/listings.html', context)
 
 
-def listing(request):
+def listing(request, listing_id):
     """Docstring"""
-    return render(request, 'listing/listing.html')
+    return render(request, 'listings/listing.html')
 
 
 def search(request):
