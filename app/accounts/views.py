688919a0ff3317128744684e568d2778f3196df7
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-13 22:19:20 +0200

    register and login templates 13-01-2024

--- app/accounts/views.py
+++ app/accounts/views.py
@@ -3,12 +3,20 @@ from django.shortcuts import render, redirect
 
 def register(request):
     """Docstring"""
-    return render(request, 'accounts/register.html')
+    if request.method == 'POST':
+        #  Register User
+        pass
+    else:
+        return render(request, 'accounts/register.html')
 
 
 def login(request):
     """Docstring"""
-    return render(request, 'accounts/login.html')
+    if request.method == 'POST':
+        #  Login User
+        pass
+    else:
+        return render(request, 'accounts/login.html')
 
 
 def logout(request):
