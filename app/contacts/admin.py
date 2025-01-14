d2c7a6caae013cfe90b8d12874d724110cc9b639
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-14 18:18:48 +0200

    Contacts Admin Customization 14-01-2024

--- app/contacts/admin.py
+++ app/contacts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin  # noqa
 
-# Register your models here.
+from .models import Contact
+
+
+class ContactAdmin(admin.ModelAdmin):
+    """Docstring for ContactAdmin."""
+    list_display = (
+        'id',
+        'name',
+        'listing',
+        'email',
+        'contact_date')
+    list_display_links = ('id', 'name')
+    search_fields = ('name', 'email', 'listing', 'contact_date')
+    list_per_page = 25
+
+
+admin.site.register(Contact, ContactAdmin)
