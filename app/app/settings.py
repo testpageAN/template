d6bd211181f9de687507ab3923dd77339f270963
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-18 15:33:33 +0200

    changes in Dockerfile, settings.py, urls.py, docker-compose.yml for media and static files 18-01-2024

--- app/app/settings.py
+++ app/app/settings.py
@@ -132,12 +132,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = '/static/'
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = '/vol/web/static'
+# STATIC_URL = '/static/'
+STATIC_URL = '/static/static/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / 'static'
-# ]
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'app/static')
 ]
@@ -151,8 +150,10 @@ AUTH_USER_MODEL = 'core.User'
 
 
 # Media folder settings
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+MEDIA_URL = '/media/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = 'vol/web/media'
 # MEDIA_URL = '/media/'
 
 # Messages
