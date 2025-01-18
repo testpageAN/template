e95763e830b4c9874591534d604fd8fad813a16c
testpageAN <alexis.nikolopoulos@hotmail.com>
2025-01-18 22:21:44 +0200

    update djamgo settings.py and docker-compose.yml 18-01-2024

--- app/app/settings.py
+++ app/app/settings.py
@@ -21,12 +21,19 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ctf%ui^@m(^+a*q2dg1sdz#fp-h90k4yb=9u19p0s7$&!h^hzw'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'changeme')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get('DEBUG', 0)))
 
 ALLOWED_HOSTS = []
+
+ALLOWED_HOSTS.extend(
+    filter(
+        None,
+        os.environ.get('ALLOWED_HOSTS', '').split(','),
+    )
+)
 
 
 # Application definition
